54db565526666d174d78382f939e3701e91dabbe
Tarik Ibrahimovic <tarikibrahimovic2016@gmail.com>
2023-10-08 23:55:56 +0200

    fetching posts

--- resources/post.py
+++ resources/post.py
@@ -12,4 +12,25 @@ blp = Blueprint('Post', __name__, description='Operations on posts')
 @blp.route('/post', methods=['POST'])
 @jwt_required()
 def post():
-    return post_service.post()
+    return post_service.upload()
+
+
+@blp.route('/all-posts', methods=['GET'])
+@blp.response(200, 'Success')
+@jwt_required()
+def all_posts():
+    return post_service.get_all_posts()
+
+
+@blp.route('/user-posts/<int:uid>', methods=['Get'])
+@blp.response(200, 'Success')
+@jwt_required()
+def user_posts(uid):
+    return post_service.get_user_posts(uid)
+
+
+@blp.route('/following-posts', methods=['GET'])
+@blp.response(200, 'Success')
+@jwt_required()
+def following_posts():
+    return post_service.get_following_posts()
--- services/post_service.py
+++ services/post_service.py
@@ -3,12 +3,13 @@ from flask_jwt_extended import get_jwt_identity
 import models
 from flask import jsonify, request
 from cloudinary.uploader import upload as cloudinary_upload
-from db import db
 from os import abort
-from sqlalchemy import and_
+
+from db import db
+from models import post
 
 
-def post():
+def upload():
     user_id = get_jwt_identity()['user_id']
     title = request.form.get('caption')
     file = request.files['file']
@@ -21,3 +22,47 @@ def post():
     except Exception as e:
         abort(500, message=str(e))
     return jsonify({"message": "Post created."}), 201
+
+
+def get_all_posts():
+    posts = db.session.execute(db.select(models.PostModel)).scalars().all()
+
+    return jsonify([{
+        "ownerUid": post.user.id,
+        "ownerUsername": post.user.username,
+        "caption": post.title,
+        "likes": post.likes,
+        "imageUrl": post.image_url,
+        "timestamp": post.created_at,
+        "ownerImageUrl": post.user.picture_url
+    } for post in posts]), 200
+
+
+def get_following_posts():
+    user_id = get_jwt_identity()['user_id']
+    following_ids = db.session.execute(db.select(models.FollowingModel.following_id)
+                                       .where(models.FollowingModel.user_id == user_id)).scalars().all()
+    posts = (db.session.execute(db.select(models.PostModel).where(models.PostModel.user_id.in_(following_ids)))
+             .scalars().all())
+    return jsonify([{
+        "ownerUid": post.user.id,
+        "ownerUsername": post.user.username,
+        "caption": post.title,
+        "likes": post.likes,
+        "imageUrl": post.image_url,
+        "timestamp": post.created_at,
+        "ownerImageUrl": post.user.picture_url
+    } for post in posts]), 200
+
+
+def get_user_posts(uid):
+    posts = db.session.execute(db.select(models.PostModel).where(models.PostModel.user_id == uid)).scalars().all()
+    return jsonify([{
+        "ownerUid": post.user.id,
+        "ownerUsername": post.user.username,
+        "caption": post.title,
+        "likes": post.likes,
+        "imageUrl": post.image_url,
+        "timestamp": post.created_at,
+        "ownerImageUrl": post.user.picture_url
+    } for post in posts]), 200
